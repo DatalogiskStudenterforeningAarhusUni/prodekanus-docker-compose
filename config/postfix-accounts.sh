0d4c0d53f9439fbca8c3ba7a58399ab802d0c4ff
Mathias Rav <rav@cs.au.dk>
2017-11-22 11:41:59 +0100

    Update postfix-accounts.sh

--- config/postfix-accounts.sh
+++ config/postfix-accounts.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-echo "# Se: https://github.com/tomav/docker-mailserver/wiki/Configure-Accounts"
+
+set -euo pipefail
+
+ACCOUNTS_INPUT=postfix-accounts/postfix-accounts.txt
+ACCOUNTS_TMP=postfix-accounts.cf.tmp
+ACCOUNTS_OUTPUT=postfix-accounts.cf
+
+if ! [ -r "$ACCOUNTS_FILENAME" ]; then
+	echo "Cannot read $ACCOUNTS_INPUT; exiting" >&2
+	exit
+fi
+
+if ! [ -w "$ACCOUNTS_OUTPUT" ]; then
+	echo "Cannot write $ACCOUNTS_OUTPUT; exiting" >&2
+	exit
+fi
+
+echo "# Se: https://github.com/tomav/docker-mailserver/wiki/Configure-Accounts" > "$ACCOUNTS_TMP"
 while read username password; do
 echo "$username|`doveadm pw -s SHA512-CRYPT -u $username -p $password`"
-done < postfix-accounts/postfix-accounts.txt
+done < "$ACCOUNTS_INPUT" >> "$ACCOUNTS_TMP"
+mv -f "$ACCOUNTS_TMP" "$ACCOUNTS_OUTPUT"
